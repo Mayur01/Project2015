98ea279d0c868b14b973de15252505c675d31147
Mriyam Tamuli <mbtamuli@gmail.com>
2015-09-09 02:17:41 +0530

    Registration Module completed initially. May require changes based on other Modules.

---
+++ Project2015/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Project2015
+{
+    class DBConnection
+    {
+        /*
+         * @param string s - takes the name of the server to connect to
+         * @param string d - takes the name of the database to use
+         *
+         * @return SqlConnection conn - A connection object to the database
+         */
+        public static SqlConnection getConn(string s, string d)
+        {
+            SqlConnection conn = new SqlConnection("server="+s+"; initial catalog="+d+ "; integrated security=true");
+            conn.Open();
+            return conn;
+        }
+    }
+}
---
+++ Project2015/Register.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Project2015
+{
+    public partial class Register : Form
+    {
+        
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox1.Clear();
+            if (!textBox1.Focused)
+            {
+                textBox1.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = DBConnection.getConn("HP-PC\\SQLEXPRESS", "project2015");
+
+            string email = textBox1.Text;
+            string passwd = textBox2.Text;
+            string saltedHashedPass = Functions.CreateHash(passwd);
+
+            String query = "INSERT INTO users(email, passwd) VALUES(@email,@passwd)";
+            SqlCommand command = new SqlCommand(query, conn);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@passwd", saltedHashedPass);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Length1:" + email.Length + "\nLength2: " + saltedHashedPass.Length);
+            MessageBox.Show("New User Created!\n User with email " + email + " created");
+        }
+
+        
+    }
+}
